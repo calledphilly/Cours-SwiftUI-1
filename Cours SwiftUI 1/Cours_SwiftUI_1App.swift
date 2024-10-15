b8a69b78fdcc3ad84c1ca1b308c68baaafa6a165
calledphilly <ybikouta@icloud.com>
2024-10-15 13:30:27 +0200

    Initial Commit

---
+++ Cours SwiftUI 1/Cours_SwiftUI_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Cours_SwiftUI_1App.swift
+//  Cours SwiftUI 1
+//
+//  Created by philly’ on 15/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Cours_SwiftUI_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cours SwiftUI 1Tests/Cours_SwiftUI_1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Cours_SwiftUI_1Tests.swift
+//  Cours SwiftUI 1Tests
+//
+//  Created by philly’ on 15/10/2024.
+//
+
+import Testing
+@testable import Cours_SwiftUI_1
+
+struct Cours_SwiftUI_1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
